dc6b82fd45253a4bba657ef6f9610e164c4f6c82
E-Liang Tan <eliang@eliangtan.com>
2020-07-13 18:37:09 +0800

    Add initial UI-backend interface object type and view models

---
+++ src/NavigatorBackendType.ts
@@ -0,0 +1,76 @@
+/**
+ * Stores the view models and interface types between the UI layer and its
+ * backend.
+ */
+
+/**
+ * A GitHub Pull Request, GitLab Merge Request, or similar.
+ */
+export interface PullRequestInfo {
+  url: string;
+  shortName: string;
+
+  isOutdated: boolean;
+}
+
+export interface Commit {
+  name: string;
+  hash: string;
+  timestamp: string;
+  author: string;
+  title: string;
+  branchNames: string[];
+  pullRequestInfo?: PullRequestInfo;
+
+  parentCommits: Commit[];
+  childCommits: Commit[];
+}
+
+export interface Repository {
+  path: string;
+  hasUncommittedChanges: boolean;
+  headHash: string;
+
+  /** The earliest interesting commit. */
+  rootDisplayCommit: Commit;
+}
+
+export type NavigatorBackend = {
+  // Getters
+  /**
+   * Asynchronously gets repository information.
+   * @param repoPath Path to repository root.
+   * @returns Cached repository information, along with a promise that returns
+   * the repository information updated with remote data (e.g. PRs).
+   */
+  getRepositoryInformation(
+    repoPath: string,
+  ): Promise<{
+    repo: Repository;
+    remoteRepoInfoPromise: Promise<Repository>;
+  }>;
+
+  // Actions
+  createOrUpdateBranchesForCommitStack(
+    repoPath: string,
+    commitStack: Commit[],
+  ): Promise<Commit[]>;
+
+  /**
+   * Uproot a commit tree and rebase it onto `targetCommit`.
+   * @param repoPath Path to repository root.
+   * @param rootCommit The commit tree root to be moved.
+   * @param targetCommit The commit the tree should be rebased on.
+   * @returns Updated `targetCommit`.
+   */
+  rebaseCommits(
+    repoPath: string,
+    rootCommit: Commit,
+    targetCommit: Commit,
+  ): Promise<Commit>;
+
+  createOrUpdatePRsForCommits(
+    repoPath: string,
+    commitStack: Commit[],
+  ): Promise<Commit[]>;
+};
