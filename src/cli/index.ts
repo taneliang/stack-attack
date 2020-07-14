f8df056e6c0a340eb28efc2d837a899ef14dde90
E-Liang Tan <eliang@eliangtan.com>
2020-07-14 17:04:26 +0800

    [Frontend][1/n] Implement initial static UI with dummy in-memory data (#4)

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,19 +2,23 @@
 import React from "react";
 import { render } from "ink";
 import meow from "meow";
+import { backend } from "../NavigatorInMemoryBackend";
 
-import App from "./ui";
+import App from "./App";
 
 const cli = meow(`
 	Usage
-	  $ sttack
-
-	Options
-		--name  Your name
+	  $ sttack [repoPath]
 
 	Examples
-	  $ sttack --name=Jane
+	  $ sttack repoPath
 	  Hello, Jane
 `);
 
-render(React.createElement(App, cli.flags));
+render(
+  React.createElement(App, {
+    // TODO: replace with Git backend
+    backend,
+    repoPath: cli.input[0] ?? process.cwd(),
+  }),
+);
