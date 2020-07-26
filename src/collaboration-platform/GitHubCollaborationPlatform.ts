ff6c5ee040e3311305cffa8941805605fd70af49
Jessie Anh Nguyen <phuonganhnguyenthi26.3@gmail.com>
2020-07-26 18:18:48 +0700

    fixed the change description function

--- src/collaboration-platform/GitHubCollaborationPlatform.ts
+++ src/collaboration-platform/GitHubCollaborationPlatform.ts
@@ -44,26 +44,21 @@ export class GitHubCollaborationPlatform implements CollaborationPlatform {
   private octokit = getOctokit(this.repoPath);
 
   async getPRForCommit(commit: Commit): Promise<PullRequestInfo | null> {
-    //{ this.owner, repo } = await repoPathToOwnerAndRepo(this.repoPath);
-    try {
-      const {
-        data,
-      } = await this.octokit.repos.listPullRequestsAssociatedWithCommit({
-        owner: this.owner,
-        repo: this.repo,
-        commit_sha: commit.hash,
-      });
-      const prResult = data[0];
-      return {
-        number: prResult.number,
-        url: prResult.url,
-        title: prResult.title,
-        description: prResult.body,
-        isOutdated: prResult.head.sha !== commit.hash,
-      };
-    } catch (e) {
-      return null;
-    }
+    const {
+      data,
+    } = await this.octokit.repos.listPullRequestsAssociatedWithCommit({
+      owner: this.owner,
+      repo: this.repo,
+      commit_sha: commit.hash,
+    });
+    const prResult = data[0];
+    return {
+      number: prResult.number,
+      url: prResult.url,
+      title: prResult.title,
+      description: prResult.body,
+      isOutdated: prResult.head.sha !== commit.hash,
+    };
   }
 
   async getPR(prNumber: PullRequestID): Promise<PullRequestInfo | null> {
@@ -89,32 +84,30 @@ export class GitHubCollaborationPlatform implements CollaborationPlatform {
     }[],
   ): Promise<Commit[]> {
     let commitArr = new Array();
-    const owner = (await repoPathToOwnerAndRepo(this.repoPath)).owner;
-    const repo = (await repoPathToOwnerAndRepo(this.repoPath)).repo;
-    commitBranchPairs.forEach(async (pair) => {
+    commitBranchPairs.forEach(async (element) => {
       //If the commit has an existing PR, don't create one, but update title + base branch
-      const prResult = await this.getPRForCommit(pair.commit);
+      const prResult = await this.getPRForCommit(element.commit);
       if (prResult !== null) {
         await this.octokit.pulls.update({
-          owner,
-          repo,
+          owner: this.owner,
+          repo: this.repo,
           pull_number: prResult.number,
-          title: pair.commit.title,
-          base: pair.baseBranch,
+          title: element.commit.title,
+          base: element.baseBranch,
         });
       }
       //Else if the commit doesn't have an existing PR, create one
       else {
         await this.octokit.pulls.create({
-          owner,
-          repo,
-          title: pair.commit.title,
-          head: pair.headBranch,
-          base: pair.baseBranch,
+          owner: this.owner,
+          repo: this.repo,
+          title: element.commit.title,
+          head: element.headBranch,
+          base: element.baseBranch,
           maintainer_can_modify: true,
         });
       }
-      commitArr.push(pair.commit);
+      commitArr.push(element.commit);
     });
     return commitArr;
   }
@@ -122,36 +115,21 @@ export class GitHubCollaborationPlatform implements CollaborationPlatform {
   async updatePRDescriptionsForCommitGraph(
     commitPrInfoPairs: { commit?: Commit; prInfo: PullRequestInfo }[],
   ): Promise<void> {
-    // Get list of PRs
-    // const pullRequests = await Promise.all(
-    //   commitPrInfoPairs.map(async ({ prInfo }) => {
-    //     const prNumber = prInfo.number;
-    //     const prResult = await this.octokit.pulls.get({
-    //       owner,
-    //       repo,
-    //       pull_number: prNumber,
-    //     });
-    //     return prResult;
-    //   }),
-    // );
-    // Update PR descriptions
     await Promise.all(
       commitPrInfoPairs.map((pullRequest, prIndex) => {
         let description =
           "Stack PR by [STACK ATTACK](https://github.com/taneliang/stack-attack):\n";
-        description += 
-          .map(({ data }, indexOfDescriptionPr) => {
+        description += commitPrInfoPairs
+          .map(({ prInfo }, indexOfDescriptionPr) => {
             const starsOrNone = prIndex === indexOfDescriptionPr ? "**" : "";
-            const number = data.number;
-            const title = data.title;
-            return `- ${starsOrNone}#${number} ${title}${starsOrNone}`;
+            return `- ${starsOrNone}#${prInfo.number} ${prInfo.title}${starsOrNone}`;
           })
           .join("\n");
 
         return this.octokit.pulls.update({
-          owner,
-          repo,
-          pull_number: pullRequest.data.number,
+          owner: this.owner,
+          repo: this.repo,
+          pull_number: pullRequest.prInfo.number,
           body: description,
         });
       }),
